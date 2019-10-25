3123ab9197258e557ef6495e97b7461db71f5874
akinko <akira.seto@gmail.com>
2019-10-25 20:42:17 +0900

    Create config.py
    configの見本ファイル

---
+++ config.py
@@ -0,0 +1,7 @@
+client_id = 'withingのid'
+client_secret = 'withingsのsecretkey'
+redirect_uri = 'withingsのredirect用uri'
+authentication_code = 'withingsのauthentication_code'
+
+train_lines = ['通勤の路線名1','通勤の路線名2']
+huebridge_ip ='huebridgeのIPアドレス'
